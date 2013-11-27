d2145d3e3b1791c268a8c414c25d1bbbc96b2196
markkalgren <mark.kalgren@gmail.com>
2013-11-27 07:10:30 -0500

    Added GitExample.cs to Doghouse

---
+++ DogHouse/GitExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHouse
+{
+    class GitExample
+    {
+        public string Color { get; set; }
+        public string Size { get; set; }
+    }
+}
